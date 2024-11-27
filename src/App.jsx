5d64f373ececa506f65905e9895cc15bcf7d75df
timmy <sparynxcyberneti@gmail.com>
2024-11-27 08:43:16 +0100

    Resolve merge conflicts

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
+import * as Sentry from '@sentry/react';
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Analytics />
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
+
+export default Sentry.withProfiler(App);
